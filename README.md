60847990b1567950ba306c43ba6c398e5cc1dbff
Mahnaz Fakhr <mahfakhr2009@gmail.com>
2022-12-19 01:00:39 +0330

    added assets and readme utils on it, comments added to exporter and README changed in project desctription and also  Task 3 descibes

--- README.md
+++ README.md
@@ -6,13 +6,14 @@
 
 ### software design
 
-<img src="./HB.png" alt="brief design" />
+<img src="./assets/systemDesign.png" alt="brief design" />
 
 ---
 
 ## system description
 
 1. app start by (yarn start or npm start)
+### index
 
 2. redis client initiate and HBExporterDependencies created base on redisclient, generated UUID and permission and also logger :
 
@@ -36,6 +37,7 @@
 
 4. exporter will be create by exporterDeps and StartExport will start working by myUser and file content which is streamed by mockOpenFile()
 
+### exporter
 ### StartExport(user,data)
 
 - 4.1. logger log the string "starting export"
@@ -54,6 +56,44 @@
 4.4 write and attach data to stream using function newCacheWriter(exportId,cache):
 
 - 4.4.1: unitl data is available data will be written chunk by chunk to exportedId by passing pending status
-- 4.4.2 by finalizing data status COMPLETE will returned and 3600 expire time will be set for exportedId key
+- 4.4.2 by finalizing data status COMPLETE will return and 3600 expire time will be set for exportedId key
 
-5.  while app is run every 500 ms GetExportStatus(id) check the status of export and will get the status based on exportId key of redis cache data
+5. while app is run every 500 ms GetExportStatus(id) check the status of export and will get the status based on exportId key of redis cache data
+
+### logger
+
+includes types: 
+```Logger``` , ```Stringable```,```ToString``` types 
+
+```NewMockLogger``` public function
+- to return a mocked log 
+
+```fieldsToString``` function
+ - to stringify optional field if its passed on it
+
+### permissions
+includes:
+
+public interface: ```PermissionsService``` 
+
+public type: ```User```
+ 
+public function ```MockPermissions``` that check permission based on permissions that passed user has
+
+###
+includes: 
+ 
+public interface: ```UUID```
+
+public function: ```MockUUIDGen``` returns generated('AAAA') UUID :D
+
+---
+
+# Task 3
+
+What would you improve? We know this feature isn't great. What would you change?
+
+1. we can have essentials or utils for some works like mockOpenFile, and sleep etc.
+2. I think having a controller will make the structure more SOLID because it is not exporter's duty to check the user permissions, before this it can be checked in controller.
+3. being more modular(permission, log, exporter) with their own services and controllers will help this structure  
+4. if I had time to check the ability of IO block vs await form of data streaming and binding in redis ,I  think we can improve it better
--- src/exporter.ts
+++ src/exporter.ts
@@ -5,6 +5,7 @@ import util from "util";
 import { UUID } from "./uuid";
 import { Logger } from "./logger";
 import { ReadStream } from "fs";
+// @interface Defined interface for Exporter to have its own type
 interface Exporter {
   StartExport: (user: User, data: Stream) => Promise<ExportStatus>;
   GetExportStatus: (id: string) => Promise<ExportStatus>;
@@ -14,11 +15,12 @@ interface Exporter {
     data: ReadStream
   ) => Promise<ExportStatus>;
 }
-
+// Define ExportStatus type as a user defined type
 type ExportStatus = {
   status: string;
   id: string;
 };
+// Define HBExporterDependencies type as a user defined type
 
 export type HBExporterDependencies = {
   cache: RedisClient;
@@ -27,11 +29,22 @@ export type HBExporterDependencies = {
   UUIDGen: UUID;
   logger: Logger;
 };
-
+/**
+ * Represents a HBExporter in the catalog.
+ * @public
+ */
 export const HBExporter = (deps: HBExporterDependencies): Exporter => {
   const get = util.promisify(deps.cache.GET).bind(deps.cache);
   const set = util.promisify(deps.cache.SET).bind(deps.cache);
   return {
+    /**
+     * start to export a file to redis cache.
+     *
+     * @param user - The user who started to export
+     * @param data - The data stream of the exporting file
+     * @returns  if process happen successfully it will return an object which has the status and id of exporting process, else it will throw error
+     *
+     */
     StartExport: async (user, data) => {
       deps.logger("starting export");
       try {
@@ -55,6 +68,13 @@ export const HBExporter = (deps: HBExporterDependencies): Exporter => {
         throw e;
       }
     },
+    /**
+     * Get status of export process
+     *
+     * @param exportId - id of exporting process
+     * @returns  if there is such an exportedId in redis cache it will return status key of ExportStatus type of exporting process , else it will throw error
+     *
+     */
     GetExportStatus: async (exportId) => {
       const strStatus = await get(exportId);
       if (!strStatus) {
@@ -63,7 +83,16 @@ export const HBExporter = (deps: HBExporterDependencies): Exporter => {
       const status: ExportStatus = JSON.parse(strStatus);
       return status;
     },
-    async CancelExport(user, id, dataStream) {
+    /**
+     * cancel export process
+     *
+     * @param user - The user who started to export
+     * @param id - id of exporting process
+     * @param data - The data stream of the exporting file
+     * @returns  if successfully run it will return an object which has the status and id of cencelled process, else it will throw error
+     *
+     */
+    async CancelExport(user, exportId, dataStream) {
       deps.logger("cancelling export");
       try {
         const allowed = await deps.permissionsService.CheckPermissions(
@@ -72,18 +101,18 @@ export const HBExporter = (deps: HBExporterDependencies): Exporter => {
         );
         if (!allowed) throw new Error(`access denied`);
 
-        const exportStatus = await this.GetExportStatus(id);
+        const exportStatus = await this.GetExportStatus(exportId);
         if (exportStatus.status) {
           throw new Error(`Export process is ${exportStatus.status}`);
         }
         const newStatus = {
           status: "CANCELED",
-          id,
+          id: exportId,
         };
         const del = util.promisify(deps.cache.DEL).bind(deps.cache) as (
           key: string
         ) => Promise<number>;
-        await del(id);
+        await del(exportId);
         dataStream.destroy();
         return newStatus;
       } catch (e) {
@@ -92,7 +121,14 @@ export const HBExporter = (deps: HBExporterDependencies): Exporter => {
     },
   };
 };
-
+/**
+ * write cache
+ *
+ * @param exportId - id of exporting process
+ * @param cache - redisClient cache
+ * @returns  a writable class that write exportId to stream and at final set status COMPLETE and set expire for redis
+ *
+ */
 function newCacheWriter(exportId: string, cache: RedisClient) {
   const append = util.promisify(cache.APPEND).bind(cache);
   const set = util.promisify(cache.SET).bind(cache);
